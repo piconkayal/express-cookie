3c43064e266a29b6a0dbd6c06d446f261ab3804f
Picon Kayal <piconn.js@gmail.com>
2022-12-01 11:00:20 +0530

    add dummy GET endpoint

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ app.use(
     express.json(),
     cors({
         credentials: true,
-        origin: ["https://codesandbox.io", "https://*.csb.app", "csb.app", "*.csb.app"],
+        origin: ["https://codesandbox.io", "https://*.csb.app", "csb.app", "*.csb.app", "https://0vvuxr.csb.app"],
         exposedHeaders: ["Set-Cookie"],
     }),
     cookieParser(),
